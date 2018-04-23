ac02488cfd9c7c837c19403d374380e10aa13ec3
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-23 20:20:07 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,7 +6,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
        t.string :location
        t.string :theme
        t.integer :price
-       t.string :family_friendly
+       t.boolean :family_friendly
        t.datetime :opening
        t.datetime :closing
        t.string :long_description
