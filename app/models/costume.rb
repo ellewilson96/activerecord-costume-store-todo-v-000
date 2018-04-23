ac041ef41006c6cd450e56b4e8cc8e7e5efd7d54
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-23 20:15:03 +0000

    automatically backed up by learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,14 +1,2 @@
-# Create your Costume class here
-# It should inherit from ActiveRecord::Base
-
 class Costume < ActiveRecord::Base
-
-  def change
-    create_table :costumes do |t|
-     t.string :name
-     t.integer :price
-     t.integer :size
-     t.string :image_url
-  end
-end
 end
