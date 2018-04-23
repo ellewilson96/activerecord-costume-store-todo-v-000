de5e1f88f20daf72038f57000d21bafffa986379
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-23 20:06:59 +0000

    automatically backed up by learn

--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,1 +1,13 @@
 # Create your HauntedHouse class here
+
+class HauntedHouse < ActiveRecord::Base
+
+  def change
+    create_table :haunted_house do |t|
+     t.string :name
+     t.integer :price
+     t.integer :size
+     t.string :image_url
+  end
+
+end
