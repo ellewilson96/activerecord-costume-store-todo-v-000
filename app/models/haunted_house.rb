2a6b4eb6cdfbc4cea02a61e3950cf3e735900351
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-23 20:15:10 +0000

    automatically backed up by learn

--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,17 +1,2 @@
-# Create your HauntedHouse class here
-
-class HauntedHouse < ActiveRecord::Migration[4.2]
-
-  def change
-    create_table :haunted_houses do |t|
-     t.string :name
-     t.string :location
-     t.string :theme
-     t.integer :price
-     t.string :family_friendly
-     t.datetime :opening
-     t.datetime :closing
-     t.string :long_description
-  end
-end
+class HauntedHouse < ActiveRecord::Base
 end
