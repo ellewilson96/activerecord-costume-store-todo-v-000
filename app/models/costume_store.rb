177486485df960a3d0e43b0dbc621690776ba0f7
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-23 20:06:39 +0000

    automatically backed up by learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,12 @@
 # Create your CostumeStore class here
+class CostumeStore < ActiveRecord::Base
+
+  def change
+    create_table :costume_store do |t|
+     t.string :name
+     t.integer :price
+     t.integer :size
+     t.string :image_url
+  end
+
+end
