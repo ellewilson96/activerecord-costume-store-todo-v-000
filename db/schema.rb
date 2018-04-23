63b64d2bee207957a3762d2f9893226eed3ad839
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-23 20:21:58 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.integer "costume_inventory"
     t.integer "employees"
     t.boolean "still_in_business"
-    t.datetime "opening_time"
-    t.datetime "closing_time"
+    t.string "opening_time"
+    t.string "closing_time"
   end
 
   create_table "costumes", force: :cascade do |t|
@@ -37,8 +37,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "theme"
     t.integer "price"
     t.boolean "family_friendly"
-    t.datetime "opening_date"
-    t.datetime "closing_date"
+    t.string "opening_date"
+    t.string "closing_date"
     t.string "description"
   end
 
