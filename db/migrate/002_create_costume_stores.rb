1024c01333c9426a8831b86facc80832860b49d0
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-23 20:16:02 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,15 @@
 # Create your costume_stores migration here
+class CostumeStore < ActiveRecord::Migration[4.2]
+
+  def change
+    create_table :costume_stores do |t|
+     t.string :name
+     t.string :locatio
+     t.integer :costume_inventory
+     t.integer :employees
+     t.string :open
+     t.datetime :opening
+     t.datetime :closing
+  end
+end
+end
